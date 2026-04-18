340dfd2e6f2ca874feff57f0c8fbbe77826c9109
luanbfranca <luanbfranca91@gmail.com>
2026-04-18 12:45:57 -0300

    feat: adiciona os dados do mock do projeto

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,11 +1,17 @@
 import { http, HttpResponse } from 'msw'
+import { CATEGORIAS_FILTRO, CATEGORIAS_NAV, NOTICIAS, TOPICOS_DESTAQUE } from './data-news'
  
 export const handlers = [
-  http.get('https://api.example.com/user', () => {
-    return HttpResponse.json({
-      id: 'abc-123',
-      firstName: 'John',
-      lastName: 'Maverick',
-    })
+  http.get('https://api.news.com/categorias', () => {
+    return HttpResponse.json(CATEGORIAS_NAV);
+  }),
+  http.get('https://api.news.com/destaque', () => {
+    return HttpResponse.json(TOPICOS_DESTAQUE);
+  }),
+  http.get('https://api.news.com/filtro', () => {
+    return HttpResponse.json(CATEGORIAS_FILTRO);
+  }),
+  http.get('https://api.news.com/noticias', () => {
+    return HttpResponse.json(NOTICIAS);
   }),
 ]
