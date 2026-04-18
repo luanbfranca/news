c7c21da20a41b5453e1c1e5253fa9995fce0743e
luanbfranca <luanbfranca91@gmail.com>
2026-04-18 12:49:05 -0300

    feat: adiciona um component para aguardar o carregamento dos mocks

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { StartMockWorker } from '@/mocks/mockServer';
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +25,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+      <body>
+        <StartMockWorker>
+          {children}
+        </StartMockWorker>
+      </body>
     </html>
   );
 }
---
+++ src/mocks/mockServer.tsx
@@ -0,0 +1,25 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+export function StartMockWorker({children}: {children: React.ReactNode}) {
+  const [isMockReady, setMockReady] = useState(false);
+
+    useEffect(() => {
+        async function enableMocks() {
+            if (process.env.NODE_ENV === 'development') {
+                const { initMocks } = await import('../mocks');
+                await initMocks();
+            }
+            setMockReady(true);
+        }
+
+        enableMocks();
+    }, []);
+
+    if (!isMockReady) {
+        return <div>Loading mocks...</div>;
+    }
+
+    return <>{children}</> ;
+}
