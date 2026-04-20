e1f62d72c1fb46c5821077c9cbd543ee2a8b80dd
luanbfranca <luanbfranca91@gmail.com>
2026-04-20 01:46:42 -0300

    feat: adiciona chamada do filtro ao service

--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -4,3 +4,8 @@ export async function getNoticias() {
     const response = await fetch('https://api.news.com/noticias');
     return await response.json();
 }
+
+export async function getFiltro() {
+    const response = await fetch('https://api.news.com/filtro');
+    return await response.json();
+}
