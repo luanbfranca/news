ca60e4110dfde55b693cb7641edaa69a27b7f92c
luanbfranca <luanbfranca91@gmail.com>
2026-04-25 12:04:19 -0300

    feat: adiciona serviço de busca por slug

--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -1,8 +1,16 @@
 'use service'
 
-export async function getNoticias() {
+import { Noticia } from '@/models/noticia';
+
+export async function getNoticias(): Promise<Noticia[]> {
     const response = await fetch('https://api.news.com/noticias');
     return await response.json();
+}
+
+export async function getNoticia(slug: string): Promise<Noticia> {
+    const response = await fetch('https://api.news.com/noticias');
+    const jsonRes = await response.json();
+    return jsonRes.filter((noticia: string) => noticia === slug);
 }
 
 export async function getFiltro() {
