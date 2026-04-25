387fd19203e8fc9af3f00f5d354391ba7eb37a66
luanbfranca <luanbfranca91@gmail.com>
2026-04-25 11:16:35 -0300

    feat: adiciona página de faq

--- src/app/faq/page.tsx
+++ src/app/faq/page.tsx
@@ -1,3 +1,48 @@
+// import { Typography } from '@mui/material';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+
 export default function FaqPage() {
-    return <div>FAQ</div>;
+    return <>
+        <Typography variant="h5" fontWeight="bold" gutterBottom component="div">
+            FAQ
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom component="div" sx={{ color: 'text.secondary', mb: 2 }}>
+            Perguntas frequentes.
+        </Typography>
+
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography component="span" fontWeight="bold">Lorem ipsum dolor</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography component="span" fontWeight="bold">Lorem ipsum dolor</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+        <Accordion>
+            <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                <Typography component="span" fontWeight="bold">Lorem ipsum dolor</Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+                <Typography>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+                    malesuada lacus ex, sit amet blandit leo lobortis eget.
+                </Typography>
+            </AccordionDetails>
+        </Accordion>
+    </>
 }
