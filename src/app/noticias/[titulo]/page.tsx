85f6ea116e5d770aa5c78d43a6bed96816d67eef
luanbfranca <luanbfranca91@gmail.com>
2026-04-25 12:23:22 -0300

    feat: adiciona página de notícia

--- src/app/noticias/[titulo]/page.tsx
+++ src/app/noticias/[titulo]/page.tsx
@@ -1,3 +1,40 @@
+'use client'
+
+import { Noticia } from '@/models/noticia';
+import { getNoticia } from '@/services/news.service';
+import { CardMedia, Typography } from '@mui/material';
+import CardMUI from '@mui/material/Card';
+import { useParams } from 'next/navigation';
+import { useEffect, useState } from 'react';
+
 export default function NoticiaPage() {
-    return <div>Notícia</div>;
+    const params = useParams<{titulo: string}>();
+    const [noticia, setNoticia] = useState<Noticia>();
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setNoticia(await getNoticia(params.titulo));
+        };
+        fetchData();
+    }, []);
+
+    return <>
+        <Typography variant="h5" fontWeight="bold" gutterBottom component="div">
+            { noticia?.title }
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom component="div" sx={{ color: 'text.secondary', mb: 2 }}>
+            { noticia?.category } • { noticia?.date }
+        </Typography>
+        <CardMUI sx={{borderRadius: '8px', height: '100%', mb: 3}}>
+            <CardMedia
+                sx={{ height: 400 }}
+                image={ noticia?.imageUrl }
+                title={ noticia?.imageAlt }
+            >
+            </CardMedia>
+        </CardMUI>
+        <Typography>
+            { noticia?.content }
+        </Typography>
+    </>
 }
