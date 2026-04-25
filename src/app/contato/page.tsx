7d0d7ffca5a7558f49b80e770379add955aabdc3
luanbfranca <luanbfranca91@gmail.com>
2026-04-25 10:42:44 -0300

    feat: adiciona página de contato

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,3 +1,62 @@
+import { Typography, TextField, Stack, InputAdornment, Button } from '@mui/material';
+import PersonIcon from '@mui/icons-material/Person';
+import EmailIcon from '@mui/icons-material/Email';
+import MessageIcon from '@mui/icons-material/Message';
+
 export default function ContatoPage() {
-    return <div>Contato</div>;
+    return <>
+        <Typography variant="h5" fontWeight="bold" gutterBottom component="div">
+            Contato
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom component="div" sx={{ color: 'text.secondary', mb: 2 }}>
+            Envie sua mensagem, sugestão ou dúvida. Responderemos o mais breve possível.
+        </Typography>
+        <Stack spacing={2} sx={{maxWidth: 500}}>
+            <TextField
+                label="Nome"
+                required
+                slotProps={{
+                    input: {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <PersonIcon />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
+            />
+            <TextField
+                label="E-mail"
+                required
+                slotProps={{
+                    input: {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <EmailIcon />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
+            />
+            <TextField
+                label="Mensagem"
+                required
+                multiline
+                rows={4}
+                slotProps={{
+                    input: {
+                        startAdornment: (
+                            <InputAdornment position="start">
+                                <MessageIcon />
+                            </InputAdornment>
+                        ),
+                    },
+                }}
+                sx={{ '& .MuiOutlinedInput-root': {'alignItems': 'flex-start'}}}
+            />
+            <div>
+                <Button variant="contained" color="secondary" sx={{ textTransform: 'none' }}>Enviar mensagem</Button>
+            </div>
+        </Stack>
+    </>
 }
