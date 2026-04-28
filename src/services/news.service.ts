ec7a3476c624d0f3e35b0da78aca065e9e954c61
luanbfranca <luanbfranca91@gmail.com>
2026-04-28 19:33:36 -0300

    refactor: remoção de código desnecessário

--- src/services/news.service.ts
+++ src/services/news.service.ts
@@ -1,10 +1,8 @@
-'use service'
-
 import { Noticia } from '@/models/noticia';
 
 export async function getNoticias(): Promise<Noticia[]> {
     const response = await fetch('https://api.news.com/noticias');
-    return await response.json();
+    return response.json();
 }
 
 export async function getNoticia(slug: string): Promise<Noticia> {
@@ -15,10 +13,10 @@ export async function getNoticia(slug: string): Promise<Noticia> {
 
 export async function getDestaques(): Promise<string[]> {
     const response = await fetch('https://api.news.com/destaque');
-    return await response.json();
+    return response.json();
 }
 
 export async function getFiltro(): Promise<string[]>  {
     const response = await fetch('https://api.news.com/filtro');
-    return await response.json();
+    return response.json();
 }
