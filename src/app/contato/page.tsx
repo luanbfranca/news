10e9dcba66ee38a3125feab98d5ac6d78a1fc3d8
luanbfranca <luanbfranca91@gmail.com>
2026-05-01 20:50:07 -0300

    feat: adiciona submissão ao formulário

--- src/app/contato/page.tsx
+++ src/app/contato/page.tsx
@@ -1,9 +1,15 @@
+'use client';
+
 import { Typography, TextField, Stack, InputAdornment, Button } from '@mui/material';
 import PersonIcon from '@mui/icons-material/Person';
 import EmailIcon from '@mui/icons-material/Email';
 import MessageIcon from '@mui/icons-material/Message';
 
 export default function ContatoPage() {
+    const onSubmit = (event: FormData) => {
+        const form = Object.fromEntries(event);
+        alert(JSON.stringify(form));
+    };
     return (
         <>
             <Typography variant="h5" fontWeight="bold" gutterBottom component="h5">
@@ -12,55 +18,60 @@ export default function ContatoPage() {
             <Typography variant="subtitle1" gutterBottom component="p" color="text.secondary" mb={2}>
                 Envie sua mensagem, sugestão ou dúvida. Responderemos o mais breve possível.
             </Typography>
-            <Stack spacing={2} maxWidth={500}>
-                <TextField
-                    label="Nome"
-                    required
-                    slotProps={{
-                        input: {
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <PersonIcon />
-                                </InputAdornment>
-                            ),
-                        },
-                    }}
-                />
-                <TextField
-                    label="E-mail"
-                    required
-                    slotProps={{
-                        input: {
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <EmailIcon />
-                                </InputAdornment>
-                            ),
-                        },
-                    }}
-                />
-                <TextField
-                    label="Mensagem"
-                    required
-                    multiline
-                    rows={4}
-                    slotProps={{
-                        input: {
-                            startAdornment: (
-                                <InputAdornment position="start">
-                                    <MessageIcon />
-                                </InputAdornment>
-                            ),
-                        },
-                    }}
-                    sx={{ '& .MuiOutlinedInput-root': { alignItems: 'flex-start' } }}
-                />
-                <div>
-                    <Button variant="contained" color="secondary" sx={{ textTransform: 'none' }}>
-                        Enviar mensagem
-                    </Button>
-                </div>
-            </Stack>
+            <form action={onSubmit}>
+                <Stack spacing={2} maxWidth={500}>
+                    <TextField
+                        name="nome"
+                        label="Nome"
+                        required
+                        slotProps={{
+                            input: {
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <PersonIcon />
+                                    </InputAdornment>
+                                ),
+                            },
+                        }}
+                    />
+                    <TextField
+                        name="email"
+                        label="E-mail"
+                        required
+                        slotProps={{
+                            input: {
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <EmailIcon />
+                                    </InputAdornment>
+                                ),
+                            },
+                        }}
+                    />
+                    <TextField
+                        name="mensagem"
+                        label="Mensagem"
+                        required
+                        multiline
+                        rows={4}
+                        slotProps={{
+                            input: {
+                                startAdornment: (
+                                    <InputAdornment position="start">
+                                        <MessageIcon />
+                                    </InputAdornment>
+                                ),
+                            },
+                        }}
+                        sx={{ '& .MuiOutlinedInput-root': { alignItems: 'flex-start' } }}
+                    />
+                    <div>
+                        <Button variant="contained" type="submit" color="secondary" sx={{ textTransform: 'none' }}>
+                            Enviar mensagem
+                        </Button>
+                    </div>
+                </Stack>
+            </form>
         </>
     );
 }
